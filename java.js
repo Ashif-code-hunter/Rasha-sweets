50454dd33fcf887c67a2f80f68782775e38aac59
hashcoder-cyber <71429125+hashcoder-cyber@users.noreply.github.com>
2021-07-06 12:00:31 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,73 @@
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop =0;
+}
+
+
+function Myfunction() {
+
+  var mybutton = document.getElementById("myBtn");
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+
+
+
+    const target = document.querySelector(".target");
+    const links = document.querySelectorAll(".mynav a");
+    const colors = ["#61045f"];
+  
+    function mouseenterFunc() {
+      if (!this.parentNode.classList.contains("active")) {
+        for (let i = 0; i < links.length; i++) {
+          if (links[i].parentNode.classList.contains("active")) {
+            links[i].parentNode.classList.remove("active");
+          }
+          links[i].style.opacity = "0.50";
+        }
+  
+        this.parentNode.classList.add("active");
+        this.style.opacity = "1";
+  
+        const width = this.getBoundingClientRect().width;
+        const height = this.getBoundingClientRect().height;
+        const left = this.getBoundingClientRect().left;
+        const top = this.getBoundingClientRect().top;
+        const color = colors[0];
+  
+        target.style.width = `${width}px`;
+        target.style.height = `${height}px`;
+        target.style.left = `${left}px`;
+        target.style.top = `${top}px`;
+        target.style.borderColor = color;
+        target.style.transform = "none";
+      }
+    }
+  
+    for (let i = 0; i < links.length; i++) {
+      //links[i].addEventListener("click", (e) => e.preventDefault());
+      links[i].addEventListener("mouseenter", mouseenterFunc);
+    }
+  
+    function resizeFunc() {
+      const active = document.querySelector(".mynav li.active");
+  
+      if (active) {
+        const left = active.getBoundingClientRect().left + window.pageXOffset;
+        const top = active.getBoundingClientRect().top + window.pageYOffset;
+  
+        target.style.left = `${left}px`;
+        target.style.top = `${top}px`;
+      }
+    }
+  
+    window.addEventListener("resize", resizeFunc);
+  
+  };
